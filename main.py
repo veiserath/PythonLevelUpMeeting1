751adc0663fce7ba7750d7f49b917c23ca5a77c7
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-04-20 17:09:44 +0200

    Task 2

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from urllib.request import Request
 from fastapi import FastAPI, Response, status
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
 import datetime
+from datetime import timedelta
 from datetime import date
+import json
 
 app = FastAPI()
-app.counter = 0
+app.counter = 1
 
 
 class Details(BaseModel):
@@ -29,9 +32,6 @@ class ModifiedDetails(BaseModel):
     surname: str
 
 
-app = FastAPI()
-
-
 @app.post("/register")
 def register(response: Response, modified_details: ModifiedDetails):
     response.status_code = status.HTTP_201_CREATED
@@ -39,8 +39,12 @@ def register(response: Response, modified_details: ModifiedDetails):
     days = len(modified_details.name) + len(modified_details.surname)
     timedifference = datetime.timedelta(days=days)
     end_date = start_date + timedifference
-    print(end_date)
-    item = Details(app.counter, modified_details.name, modified_details.surname, start_date, end_date)
-    json_compatible_item_data = jsonable_encoder(item)
+    start_date = start_date.strftime("%Y-%m-%d")
+    end_date = end_date.strftime("%Y-%m-%d")
+    data_set = {"id": app.counter, "name": modified_details.name, "surname": modified_details.surname,
+                "register_date": start_date, "vaccination_date": end_date}
+    json_dump = json.dumps(data_set)
+    # item = Details(app.counter, modified_details.name, modified_details.surname, start_date, end_date)
+    # json_compatible_item_data = jsonable_encoder(item)
     app.counter += 1
-    return JSONResponse(content=json_compatible_item_data)
+    return json_dump
