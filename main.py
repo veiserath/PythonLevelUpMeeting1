e3e218df3de97f29799e40825af5491d38757334
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-04-20 18:04:03 +0200

    Task 2

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from urllib.request import Request
 from fastapi import FastAPI, Response, status
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from starlette.responses import JSONResponse
 import datetime
-from datetime import timedelta
 from datetime import date
-import json
 
 app = FastAPI()
 app.counter = 1
+app.patients = list()
 
 
 class Details(BaseModel):
@@ -37,14 +33,37 @@ def register(response: Response, modified_details: ModifiedDetails):
     response.status_code = status.HTTP_201_CREATED
     start_date = date.today()
     days = len(modified_details.name) + len(modified_details.surname)
+    print(days)
     timedifference = datetime.timedelta(days=days)
     end_date = start_date + timedifference
     start_date = start_date.strftime("%Y-%m-%d")
     end_date = end_date.strftime("%Y-%m-%d")
     data_set = {"id": app.counter, "name": modified_details.name, "surname": modified_details.surname,
                 "register_date": start_date, "vaccination_date": end_date}
-    # json_dump = json.dumps(data_set)
-    # item = Details(app.counter, modified_details.name, modified_details.surname, start_date, end_date)
-    # json_compatible_item_data = jsonable_encoder(item)
+    app.patients.append(data_set)
     app.counter += 1
     return data_set
+
+
+@app.get("/patient/{id}")
+def patient(response: Response, id):
+    response.status_code = status.HTTP_200_OK
+    for dictionary in app.patients:
+        if dictionary["id"] == id:
+            return dictionary
+        if dictionary["id"] < 1:
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return
+
+
+# some_list = list()
+#
+# data_set = {"id": 1, "name": "Michal", "surname": "Markowicz",
+#                 "register_date": "2021-04-20", "vaccination_date": "2021-05-06"}
+#
+# some_list.append(data_set)
+# for dictionary in some_list:
+#         if dictionary["id"] == 1:
+#             print(dictionary)
