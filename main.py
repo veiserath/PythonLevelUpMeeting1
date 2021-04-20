b7adea38cb3c33c58295568b81823ab7f89d209f
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-04-20 15:06:19 +0200

    Task 2

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from fastapi import FastAPI,Response,status
 app = FastAPI()
 
 
-@app.get("/auth", status_code=204)
+@app.get("/auth")
 def get(password: str, password_hash: str, response: Response):
+    response.status_code = status.HTTP_401_UNAUTHORIZED
     encrypted = hashlib.sha512(str(password).encode("utf-8")).hexdigest()
     if encrypted == password_hash:
         response.status_code = status.HTTP_204_NO_CONTENT
         return
-    response.status_code = status.HTTP_401_UNAUTHORIZED
     return
