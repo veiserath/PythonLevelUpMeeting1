7ebd19e598a592bfda01e2add644cb58892c7362
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-04-20 13:34:09 +0200

    Task 2

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello world!"}
+@app.get("/method")
+def get():
+    return {"method": "GET"}
+@app.put("/method")
+def put():
+    return {"method": "PUT"}
+@app.options("/method")
+def options():
+    return {"method": "OPTIONS"}
+@app.delete("/method")
+def delete():
+    return {"method": "DELETE"}
+@app.post("/method", status_code=201)
+def post():
+    return {"method": "POST"}
