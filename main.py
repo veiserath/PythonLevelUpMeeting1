28e3de5192d7d21a9ac10032c43ebd9e8bb56aaa
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-04-21 16:12:23 +0200

    Task 5

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
+import hashlib
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 import datetime
 from datetime import date
 
-app = FastAPI()
-app.counter = 1
+ app = FastAPI()
+ app.counter = 1
 
 
-class Details(BaseModel):
-    id: int
-    name: str
-    surname: str
-    register_date: str
-    vaccination_date: str
+ class Details(BaseModel):
+     id: int
+     name: str
+     surname: str
+     register_date: str
+     vaccination_date: str
 
-    def __init__(self, id, name, surname, register_date, vaccination_date):
-        self.id = id
-        self.name = name
-        self.surname = surname
-        self.register_date = register_date
-        self.vaccination_date = vaccination_date
+     def __init__(self, id, name, surname, register_date, vaccination_date):
+         self.id = id
+         self.name = name
+         self.surname = surname
+         self.register_date = register_date
+         self.vaccination_date = vaccination_date
 
 
-class ModifiedDetails(BaseModel):
-    name: str
-    surname: str
+ class ModifiedDetails(BaseModel):
+     name: str
+     surname: str
+
+
+ @app.get("/auth")
+ def get(response: Response, password: str = None, password_hash: str = None):
+     response.status_code = status.HTTP_401_UNAUTHORIZED
+     if password is None or password_hash is None or password is "" or password_hash is "":
+         return
+     encrypted = hashlib.sha512(str(password).encode("utf-8")).hexdigest()
+     if encrypted == password_hash:
+         response.status_code = status.HTTP_204_NO_CONTENT
+         return
+     else:
+         response.status_code = status.HTTP_401_UNAUTHORIZED
+         return
 
 
 @app.post("/register")
@@ -32,21 +47,12 @@ def register(response: Response, modified_details: ModifiedDetails):
     response.status_code = status.HTTP_201_CREATED
     start_date = date.today()
     counter = 0
-    i = 0
     for character in modified_details.name:
-        if i == 0 and 'A' <= character <= 'Z':
+        if 'A' <= character <= 'Z' or 'a' <= character <= 'z':
             counter += 1
-            i += 1
-        elif 'a' <= character <= 'z':
-            counter += 1
-        i += 1
-    i = 0
     for character in modified_details.surname:
-        if i == 0 and 'A' <= character <= 'Z':
+        if 'A' <= character <= 'Z' or 'a' <= character <= 'z':
             counter += 1
-        elif 'a' <= character <= 'z':
-            counter += 1
-        i += 1
     timedifference = datetime.timedelta(days=counter)
     end_date = start_date + timedifference
     start_date = start_date.strftime("%Y-%m-%d")
