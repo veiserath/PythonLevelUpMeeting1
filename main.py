b9c4ace0e07f279e41db4d52e8b581c3ee76a445
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-04-21 16:17:29 +0200

    Task 5

--- main.py
+++ main.py
@@ -4,42 +4,42 @@ from pydantic import BaseModel
 import datetime
 from datetime import date
 
- app = FastAPI()
- app.counter = 1
+app = FastAPI()
+app.counter = 1
 
 
- class Details(BaseModel):
-     id: int
-     name: str
-     surname: str
-     register_date: str
-     vaccination_date: str
-
-     def __init__(self, id, name, surname, register_date, vaccination_date):
-         self.id = id
-         self.name = name
-         self.surname = surname
-         self.register_date = register_date
-         self.vaccination_date = vaccination_date
+class Details(BaseModel):
+    id: int
+    name: str
+    surname: str
+    register_date: str
+    vaccination_date: str
 
 
- class ModifiedDetails(BaseModel):
-     name: str
-     surname: str
+def __init__(self, id, name, surname, register_date, vaccination_date):
+    self.id = id
+    self.name = name
+    self.surname = surname
+    self.register_date = register_date
+    self.vaccination_date = vaccination_date
 
 
- @app.get("/auth")
- def get(response: Response, password: str = None, password_hash: str = None):
-     response.status_code = status.HTTP_401_UNAUTHORIZED
-     if password is None or password_hash is None or password is "" or password_hash is "":
-         return
-     encrypted = hashlib.sha512(str(password).encode("utf-8")).hexdigest()
-     if encrypted == password_hash:
-         response.status_code = status.HTTP_204_NO_CONTENT
-         return
-     else:
-         response.status_code = status.HTTP_401_UNAUTHORIZED
-         return
+class ModifiedDetails(BaseModel):
+    name: str
+    surname: str
+
+
+@app.get("/auth")
+def get(response: Response, password: str = None, password_hash: str = None):
+    response.status_code = status.HTTP_401_UNAUTHORIZED
+    if password is None or password_hash is None or password is "" or password_hash is "":
+        return
+    encrypted = hashlib.sha512(str(password).encode("utf-8")).hexdigest()
+    if encrypted == password_hash:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return
+    response.status_code = status.HTTP_401_UNAUTHORIZED
+    return
 
 
 @app.post("/register")
