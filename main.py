063fe0a5501d317c43d8b0a8139174c55e50cd88
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-05-04 18:33:56 +0200

    Zad 3.4, proba 5

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Depends, HTTPException, status, Cookie, Response, Query
+from fastapi import FastAPI, Request, Depends, HTTPException, status, Cookie, Response,Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
@@ -17,12 +17,10 @@ security = HTTPBasic()
 
 templates = Jinja2Templates(directory='templates')
 
-
-@app.get("/hello", response_class=HTMLResponse)
+@app.get("/hello", response_class = HTMLResponse)
 def root(request: Request):
     today = date.today().strftime("%Y-%m-%d")
     return templates.TemplateResponse('index.html', {'request': request, 'today': today})
-
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, "4dm1n")
@@ -37,7 +35,7 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
 
 
 @app.post("/login_session")
-def read_current_user(response: Response, username=Depends(get_current_username)):
+def read_current_user(response:Response, username = Depends(get_current_username)):
     # app.tokens_login_session.clear()
     # app.tokens_login_session.append('abc')
     token_value1.append('z')
@@ -51,14 +49,14 @@ def read_current_user(response: Response, username=Depends(get_current_username)
         app.tokens_login_session[1] = app.tokens_login_session[2]
         app.tokens_login_session[2] = str1
     print(f'{str1}')
-    response.set_cookie(key='session_token', value=str1)
+    response.set_cookie(key = 'session_token', value = str1)
     response.status_code = status.HTTP_201_CREATED
     print(f'{app.tokens_login_session}')
     return {'username': username}
 
 
 @app.post("/login_token")
-def read_current_user(response: Response, username=Depends(get_current_username)):
+def read_current_user(response:Response, username = Depends(get_current_username)):
     token_value.append('a')
     str1 = ''
     for i in token_value:
@@ -70,11 +68,10 @@ def read_current_user(response: Response, username=Depends(get_current_username)
         app.tokens_login_token[1] = app.tokens_login_token[2]
         app.tokens_login_token[2] = str1
 
-    response.set_cookie(key="session_token", value=str1)
+    response.set_cookie(key="session_token", value= str1)
     response.status_code = status.HTTP_201_CREATED
     print(f'{app.tokens_login_token}')
     return {'token': str1}
-
 
 @app.get("/welcome_session")
 def func(*, response: Response, session_token: str = Cookie(None), format: Optional[str] = None):
@@ -96,7 +93,6 @@ def func(*, response: Response, session_token: str = Cookie(None), format: Optio
     else:
         return PlainTextResponse("Welcome!")
 
-
 @app.get("/welcome_token")
 def func(token: List[str] = Query(None), format: Optional[str] = None):
     if [i for i in token if i in app.tokens_login_token] == []:
@@ -116,30 +112,29 @@ def func(token: List[str] = Query(None), format: Optional[str] = None):
     else:
         return PlainTextResponse("Welcome!")
 
-
-@app.delete("/logout_session")
-def logout_session(response: Response, session_token: str = Cookie(None), format: Optional[str] = None):
+@app.delete('/logout_session')
+def func(*, response: Response,  session_token: str = Cookie(None), format: Optional[str] = None):
     if session_token not in app.tokens_login_session:
-        response.status_code = 401
-        return response
+        raise HTTPException(status_code=401, detail="Unathorised")
     app.tokens_login_session.remove(session_token)
+    print(f'{app.tokens_login_session}')
     url = f'/logged_out?format={format}'
-    response.status_code = 302
-    return RedirectResponse(url=url, status_code=302)
+    return RedirectResponse(url = url, status_code = 303)
 
-
-@app.delete("/logout_token")
-def logout_token(response: Response, token: List[str] = Query(None), format: Optional[str] = None):
-    if not [i for i in token if i in app.tokens_login_token]:
-        raise HTTPException(status_code=401, detail="Unauthorised")
+@app.delete('/logout_token')
+def func(request: Request, token: str = Query(None), format: Optional[str] = None):
+    #if [i for i in token if i in app.tokens_login_token] == []:
+    if token not in app.tokens_login_token:
+        raise HTTPException(status_code=401, detail="Unathorised")
+    print(f'app.tokens_login_token: {app.tokens_login_token}')
+    print(f'token: {token}')
     app.tokens_login_token.remove(token)
+    print(f'app.tokens_login_token: {app.tokens_login_token}')
     url = f'/logged_out?format={format}'
-    response.status_code = 302
-    return RedirectResponse(url=url, status_code=302)
+    return RedirectResponse(url = url, status_code = 303)
 
-
-@app.get("/logged_out")
-def logged_out(format: Optional[str] = None):
+@app.get('/logged_out')
+def func(format: Optional[str] = None):
     if format == 'json':
         return JSONResponse({"message": "Logged out!"})
     if format == 'html':
@@ -154,4 +149,3 @@ def logged_out(format: Optional[str] = None):
     """)
     else:
         return PlainTextResponse("Logged out!")
-
