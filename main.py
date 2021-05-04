488f97266d8708efc3754d896f8f3c506551a555
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-05-04 17:12:31 +0200

    Zad 3.2, proba 4

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-from hashlib import sha256
-from fastapi import FastAPI, Response, Cookie, HTTPException, status, Depends
+from fastapi import FastAPI, Response, Cookie, HTTPException, Request, Depends
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import uvicorn
 
 app = FastAPI()
-app.secret_key = "very constatn and random secret, best 64+ characters"
-app.access_tokens = []
+
+app.secret_key_sample = 'qwerty'
+app.composition_to_key = 1
+app.session_token = ''
 
 security = HTTPBasic()
 
 
-@app.post("/login_session")
-def login(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
-    user = credentials.username
+@app.post("/login_session", status_code=201)
+def create_login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+    login = credentials.username
     password = credentials.password
-    if user == "4dm1n" and password == "NotSoSecurePa$$":
-        session_token = sha256(f"{user}{password}{app.secret_key}".encode()).hexdigest()
-        app.access_tokens.append(session_token)
-        response.set_cookie(key="session_token", value=session_token)
-        response.status_code = status.HTTP_201_CREATED
-        return response
+    if login == '4dm1n' and password == 'NotSoSecurePa$$':
+        session_token = app.secret_key_sample + str(app.composition_to_key)
+        app.session_token = session_token
+        app.composition_to_key += 1
+        response.set_cookie(key='session_token', value=session_token)
     else:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
+        response.status_code = 401
         return response
 
 
-@app.post("/login_token")
-def secured_data(response: Response, session_token: str = Cookie(None),
-                 credentials: HTTPBasicCredentials = Depends(security)):
-    user = credentials.username
+@app.post("/login_token", status_code=201)
+def get_login_token(response: Response, session_token: str = Cookie(None)
+                    , credentials: HTTPBasicCredentials = Depends(security)):
+    login = credentials.username
     password = credentials.password
-    if user == '4dm1n' and password == 'NotSoSecurePa$$':
+    if login == '4dm1n' and password == 'NotSoSecurePa$$':
         session_token = app.secret_key_sample + str(app.composition_to_key)
         app.session_token = session_token
         app.composition_to_key += 1
         response.set_cookie(key='session_token', value=session_token)
         return {"token": app.session_token}
-    if session_token in app.access_tokens:
-        app.access_tokens.remove(session_token)
-        response.status_code = status.HTTP_201_CREATED
-        return {"token": session_token}
-    if session_token not in app.access_tokens:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-        return response
+
+    if session_token == app.session_token:
+        return {"token": app.session_token}
+
+    response.status_code = 401
+    return response
