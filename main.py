f837dc66929339e50ef39f73092af1e856f3d7e7
Kacper Młodkowski <k.mlodkowski@gmail.com>
2021-05-11 21:48:24 +0200

    First exercise

--- main.py
+++ main.py
@@ -1,151 +1,46 @@
-from fastapi import FastAPI, Request, Depends, HTTPException, status, Cookie, Response,Query
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from typing import List, Optional
-import secrets
-from datetime import date
-from fastapi.responses import JSONResponse, HTMLResponse, PlainTextResponse, RedirectResponse
+import sqlite3
+from fastapi import FastAPI, Response, Cookie, HTTPException, Request, Depends
+import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
-# app.secret_key = "very constatn and random secret, best 64+ characters"
-app.tokens_login_token = []
-app.tokens_login_session = []
-token_value = []
-token_value1 = []
-security = HTTPBasic()
-
-templates = Jinja2Templates(directory='templates')
-
-@app.get("/hello", response_class = HTMLResponse)
-def root(request: Request):
-    today = date.today().strftime("%Y-%m-%d")
-    return templates.TemplateResponse('index.html', {'request': request, 'today': today})
-
-def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "4dm1n")
-    correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
+app.db_connection = None
 
 
-@app.post("/login_session")
-def read_current_user(response:Response, username = Depends(get_current_username)):
-    # app.tokens_login_session.clear()
-    # app.tokens_login_session.append('abc')
-    token_value1.append('z')
-    str1 = ''
-    for i in token_value1:
-        str1 += 'z'
-    if len(app.tokens_login_session) < 3:
-        app.tokens_login_session.append(str1)
-    else:
-        app.tokens_login_session[0] = app.tokens_login_session[1]
-        app.tokens_login_session[1] = app.tokens_login_session[2]
-        app.tokens_login_session[2] = str1
-    print(f'{str1}')
-    response.set_cookie(key = 'session_token', value = str1)
-    response.status_code = status.HTTP_201_CREATED
-    print(f'{app.tokens_login_session}')
-    return {'username': username}
+@app.on_event("startup")
+async def startup():
+    app.db_connection = sqlite3.connect("northwind.db")
+    app.db_connection.text_factory = lambda b: b.decode(errors="ignore")  # northwind specific
 
 
-@app.post("/login_token")
-def read_current_user(response:Response, username = Depends(get_current_username)):
-    token_value.append('a')
-    str1 = ''
-    for i in token_value:
-        str1 += 'a'
-    if len(app.tokens_login_token) < 3:
-        app.tokens_login_token.append(str1)
-    else:
-        app.tokens_login_token[0] = app.tokens_login_token[1]
-        app.tokens_login_token[1] = app.tokens_login_token[2]
-        app.tokens_login_token[2] = str1
+@app.on_event("shutdown")
+async def shutdown():
+    app.db_connection.close()
 
-    response.set_cookie(key="session_token", value= str1)
-    response.status_code = status.HTTP_201_CREATED
-    print(f'{app.tokens_login_token}')
-    return {'token': str1}
 
-@app.get("/welcome_session")
-def func(*, response: Response, session_token: str = Cookie(None), format: Optional[str] = None):
-    print(f'{app.tokens_login_session}')
-    if session_token not in app.tokens_login_session:
-        raise HTTPException(status_code=401, detail="Unathorised")
-    if format == 'json':
-        return JSONResponse({"message": "Welcome!"})
-    if format == 'html':
-        return HTMLResponse("""
-    <html>
-        <head>
-        </head>
-        <body>
-            <h1>Welcome!</h1>
-        </body>
-    </html>
-    """)
-    else:
-        return PlainTextResponse("Welcome!")
+@app.get("/categories", status_code=200)
+async def get_categories():
+    conn = app.db_connection
+    cursor = conn.cursor()
+    cursor.row_factory = sqlite3.Row
+    categories = cursor.execute(""
+                                "select categoryid id, categoryname name "
+                                "from categories "
+                                "order by categoryid").fetchall()
+    return dict(categories=categories)
 
-@app.get("/welcome_token")
-def func(token: List[str] = Query(None), format: Optional[str] = None):
-    if [i for i in token if i in app.tokens_login_token] == []:
-        raise HTTPException(status_code=401, detail="Unathorised")
-    if format == 'json':
-        return JSONResponse({"message": "Welcome!"})
-    if format == 'html':
-        return HTMLResponse("""
-    <html>
-        <head>
-        </head>
-        <body>
-            <h1>Welcome!</h1>
-        </body>
-    </html>
-    """)
-    else:
-        return PlainTextResponse("Welcome!")
 
-@app.delete('/logout_session')
-def func(*, response: Response,  session_token: str = Cookie(None), format: Optional[str] = None):
-    if session_token not in app.tokens_login_session:
-        raise HTTPException(status_code=401, detail="Unathorised")
-    app.tokens_login_session.remove(session_token)
-    print(f'{app.tokens_login_session}')
-    url = f'/logged_out?format={format}'
-    return RedirectResponse(url = url, status_code = 303)
-
-@app.delete('/logout_token')
-def func(request: Request, token: str = Query(None), format: Optional[str] = None):
-    #if [i for i in token if i in app.tokens_login_token] == []:
-    if token not in app.tokens_login_token:
-        raise HTTPException(status_code=401, detail="Unathorised")
-    print(f'app.tokens_login_token: {app.tokens_login_token}')
-    print(f'token: {token}')
-    app.tokens_login_token.remove(token)
-    print(f'app.tokens_login_token: {app.tokens_login_token}')
-    url = f'/logged_out?format={format}'
-    return RedirectResponse(url = url, status_code = 303)
-
-@app.get('/logged_out')
-def func(format: Optional[str] = None):
-    if format == 'json':
-        return JSONResponse({"message": "Logged out!"})
-    if format == 'html':
-        return HTMLResponse("""
-    <html>
-        <head>
-        </head>
-        <body>
-            <h1>Logged out!</h1>
-        </body>
-    </html>
-    """)
-    else:
-        return PlainTextResponse("Logged out!")
+@app.get("/customers", status_code=200)
+async def get_customers():
+    conn = app.db_connection
+    cursor = conn.cursor()
+    cursor.row_factory = sqlite3.Row
+    customers = cursor.execute(
+        "SELECT CustomerID id, COALESCE(CompanyName, '') name, "
+        "COALESCE(Address, '') || ' ' || COALESCE(PostalCode, '') || ' ' || COALESCE(City, "
+        "'') || "
+        "' ' || COALESCE(Country, '') full_address "
+        "from customers "
+        "ORDER BY UPPER(CustomerID);"
+    ).fetchall()
+    return dict(customers=customers)
